db68e411e4e0f6066e09e40ed7ae5f505f5e0d4d
Miccy <code@miccy.dev>
2026-03-01 18:13:15 +0100

    sync(common-v8): defer worker polyfill bootstrap in web runtime
    Reverts the wave1 worker bootstrap equivalent from a4287569 because it breaks @evolu/web webkit worker tests (Object not disposable).
    
    Deferred to wave2 runtime refactor.

--- packages/web/src/local-first/Worker.worker.ts
+++ packages/web/src/local-first/Worker.worker.ts
@@ -1,12 +1,9 @@
 /// <reference lib="webworker" />
 declare const self: SharedWorkerGlobalScope;
 
-import { installPolyfills } from "@evolu/common";
 import { initEvoluWorker } from "@evolu/common/local-first";
 import { createSharedWorkerScope, createWorkerRun } from "../Worker.js";
 import { runWebDbWorkerPort } from "./DbWorker.js";
-
-installPolyfills();
 
 await using baseRun = createWorkerRun();
 const run = baseRun.addDeps({ runDbWorkerPort: runWebDbWorkerPort });
